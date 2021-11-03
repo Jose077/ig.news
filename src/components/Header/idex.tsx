8b5b6c1c4c7cbab68ba47a0dc1d7ea9a62915096
jose <josesandeson54@gmail.com>
2021-11-03 16:10:09 -0300

    :sparkles: Criado header da aplicacao

---
+++ src/components/Header/idex.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { SignInButton } from '../SigninButton'
+import styles from './styles.module.scss'
+
+export function Header(){
+    return (
+        <header className={styles.headerContainer}>
+            <div className={styles.headerContent}> 
+                <img src="/images/logo.svg" alt="ig.news" />
+
+                <nav>
+                    <a href="/" > Home </a>
+                    <a href="/"> Posts </a>
+                </nav>
+
+                <SignInButton />
+            </div>
+        </header>
+    )
+}
