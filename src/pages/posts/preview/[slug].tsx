71ca5c65e2e276128f82b7ea8aea155a621c050c
jose <josesandeson54@gmail.com>
2021-12-08 16:19:14 -0300

    Testes da pagina de preview

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -23,6 +23,7 @@ export default function PostPreview({ post }: PostPreviewProps ){
 
     useEffect(() => {
         if(session?.activeSubscription) {
+            
             router.push(`/posts/${post.slug}`)
         }
     }, [session])
---
+++ src/tests/pages/PostPreview.spec.tsx
@@ -0,0 +1,58 @@
+import { render, screen } from "@testing-library/react";
+import Post from "../../pages/posts/preview/[slug]";
+import { mocked } from "ts-jest/utils";
+import {  useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+
+
+
+const post = {
+    slug: 'my-new-post',
+    title: 'My new post',
+    content: '<p> post excerpt </p> ',
+    updatedAt: '10 de Abril'
+}
+
+jest.mock('next-auth/client');
+// jest.mock('../../services/prismic');
+jest.mock('next/router')
+
+describe('Post preview page', () => {
+
+    it('renders correctly', () => {
+        const useSessionMocked = mocked(useSession);
+
+        useSessionMocked.mockReturnValueOnce([null, false])
+
+        render(
+            <Post post={post} />
+        );
+
+        expect(screen.getByText("My new post")).toBeInTheDocument();
+        expect(screen.getByText("post excerpt")).toBeInTheDocument();
+        expect(screen.getByText("Wanna continue reading?")).toBeInTheDocument();
+    });
+
+    it('redirects user to full post when user is subscribe', async () => {
+        const useSessionMocked = mocked(useSession);
+        const useRouterMocked = mocked(useRouter);
+        const pushMock = jest.fn();
+
+        useSessionMocked.mockReturnValueOnce([
+            { activeSubscription: 'fake-active-subscription' },
+            false
+        ] as any);
+
+        useRouterMocked.mockReturnValueOnce({
+            push: pushMock,
+        } as any);
+
+        render(<Post post={post} />);
+        
+
+        expect(pushMock).toHaveBeenCalledWith('/posts/my-new-post')
+
+    });
+
+
+});
