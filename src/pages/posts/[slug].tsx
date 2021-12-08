a277fd9c4f5a7e6f80cb87bcda0c27f1fccd6938
jose <josesandeson54@gmail.com>
2021-12-08 15:27:22 -0300

    criados os testes da pagina post

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -59,7 +59,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
     
     const post = {
         slug,
-        ttle: RichText.asText(response?.data.title),
+        title: RichText.asText(response?.data.title),
         content: RichText.asHtml(response?.data.content),
         updatedAt: new Date(response?.last_publication_date).toLocaleDateString('pt-BR', {
             day: '2-digit',
